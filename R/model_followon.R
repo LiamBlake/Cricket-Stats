a337e4770e14baba330950508ae4efc64e6c4b0c
LiamBlake <liamlemblake@gmail.com>
2021-02-25 08:03:18 +1030

    logistic reg

--- R/model_followon.R
+++ R/model_followon.R
@@ -4,9 +4,11 @@
 
 
 # Load packages and data
-pacman::p_load(tidyverse, tidymodels, visNetwork)
+pacman::p_load(tidyverse, tidymodels, vip, tidyroc)
 fo <- readRDS("follow_ons.rds") %>% mutate_at(c("follow_on"), as.factor) %>% filter(lead <= -200)
 
+
+models <- data.frame()
 
 # First model - decision tree
 # Set up workflow
@@ -17,10 +19,6 @@ wf_tree <- workflow() %>%
                       add_model(
                         decision_tree(mode = 'classification', tree_depth = tune()) %>%
                           set_engine('rpart'))
-
-
-fit_tree <- wf_tree %>% fit(data = fo) %>% pull_workflow_fit()
-visTree(fit_tree$fit)
 
 
 # Tune tree depth
@@ -37,3 +35,14 @@ show_best(tune_tree, metric = "accuracy", n = 10)
 
 final_tree <- wf_tree %>% finalize_workflow(M1) %>% fit(data = fo)
 final_fit <- final_tree %>% pull_workflow_fit()
+
+
+
+# Second model - logistic regression
+logis_fit <- logistic_reg() %>% set_engine("glm") %>% fit(follow_on ~ ., data = fo) 
+
+tmp <- fo %>% add_column(logis_fit %>% predict(new_data = fo)) 
+tmp %>% make_roc(predictor = tmp$.pred_class, known_class = follow_on)
+
+
+models <- rbind(models, data.frame())
